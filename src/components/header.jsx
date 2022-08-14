4ba9b86551e0582c098a4ca551fc8b5769ae2fa3
Simon Mollet <mollet.simon.pro@gmail.com>
2022-08-15 01:50:56 +0200

    update favicon and title

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,8 +4,6 @@ export default function Header() {
   return (
     <nav
       className="w-full flex justify-end p-10 scroll-smooth"
-      data-aos="flip-up"
-      data-aos-once="true"
     >
       <a
         className="text-nav tracking-widest mr-16 hover:line-through cursor-pointer"
